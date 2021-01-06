ee538455df3d607bc6e0d5925ec8c0e7389e3913
VitorAntonio644 <76141274+VitorAntonio644@users.noreply.github.com>
2021-01-06 11:09:14 -0300

    Update Paciente.cs

--- OdontoTech/Domain/Paciente.cs
+++ OdontoTech/Domain/Paciente.cs
@@ -8,6 +8,14 @@ namespace Domain
 {
     class Paciente
     {
+        public int idPaciente { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Rg { get; set; }
+        public string Cpf { get; set; }
+        public string dtNascimento { get; set; } // Data esta como string ! (TEMPORARIO)
+        public string obs { get; set; }
+        public int idEndereco { get; set; }
 
     }
 }
