58848cb62b5b0a85468e185b8d3dc4f8686c4ff3
marcieleadivincula <marcieleadivincula@gmail.com>
2021-01-06 11:33:39 -0300

    Create class Clinica

---
+++ OdontoTech/Domain/Clinica.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Clinica
+    {
+        public int Id { get; set; }
+        public string nNome { get; set; }
+        public DateTime DataInauguracao { get; set; } //verificar qual usar para apenas DATA
+        public Endereco Endereco { get; set; }
+
+        public Clinica(int id, string nNome, DateTime dataInauguracao, Endereco endereco)
+        {
+            Id = id;
+            this.nNome = nNome;
+            DataInauguracao = dataInauguracao;
+            Endereco = endereco;
+        }
+    }
+}
