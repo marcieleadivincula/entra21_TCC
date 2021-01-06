d4fa2ef50e1c4086e5736b5d7d1998adb59e6522
marcieleadivincula <marcieleadivincula@gmail.com>
2021-01-06 11:33:13 -0300

    Create class Atendimento

---
+++ OdontoTech/Domain/Atendimento.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Atendimento
+    {
+        public int Id { get; set; }
+        public Paciente Paciente { get; set; }
+        public Colaborador Colaborador { get; set; }
+        public List<Procedimento> Procedimentos = new List<Procedimento>();
+
+        public Atendimento(int id, Paciente paciente, Colaborador colaborador, List<Procedimento> procedimentos)
+        {
+            this.Id = id;
+            this.Paciente = paciente;
+            this.Colaborador = colaborador;
+            this.Procedimentos = procedimentos;
+        }
+
+        public Atendimento(int id, Paciente paciente, Colaborador colaborador)
+        {
+            this.Id = id;
+            this.Paciente = paciente;
+            this.Colaborador = colaborador;
+        }
+    }
+}
