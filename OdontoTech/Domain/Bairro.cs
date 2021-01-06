5c4ee5c24cda92cfdb6485c3920dd3b84ed30aac
marcieleadivincula <marcieleadivincula@gmail.com>
2021-01-06 11:33:22 -0300

    Create class Bairro

---
+++ OdontoTech/Domain/Bairro.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Bairro
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int idCidade { get; set; }
+
+        public Bairro(int id, string nome, int idCidade)
+        {
+            Id = id;
+            Nome = nome;
+            this.idCidade = idCidade;
+        }
+
+    }
+}
