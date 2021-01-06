57d68696b3c460981a390034de18f0ad839555e3
marcieleadivincula <marcieleadivincula@gmail.com>
2021-01-06 10:28:30 -0300

    Criado estrutura inicial do projeto

---
+++ OdontoTech/DataAccessLayer/DBConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class DBConfig
+    {
+        public const string CONNECTION_STRING = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Diuari\Desktop\OldIron\Entra21\OldIronFinalSolution\DataAccessLayer\FerroVelhoDB.mdf;Integrated Security=True;Connect Timeout=30";
+    }
+}
