c71ae105cd7a9757bf2a04448e4441e4cbcc9af1
VitorAntonio644 <76141274+VitorAntonio644@users.noreply.github.com>
2021-01-06 10:37:46 -0300

    Update DBConfig.cs
    Funçoes  na classe do SQL

--- OdontoTech/DataAccessLayer/DBConfig.cs
+++ OdontoTech/DataAccessLayer/DBConfig.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace DataAccessLayer
 {
     public class DBConfig
     {
-        public const string CONNECTION_STRING = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Diuari\Desktop\OldIron\Entra21\OldIronFinalSolution\DataAccessLayer\FerroVelhoDB.mdf;Integrated Security=True;Connect Timeout=30";
+        public const string CONNECTION_STRING = @"server=localhost;user id=root;database=sorrisempre";
+        public SqlCommand Cmd;
+        public SqlDataReader Reader;
     }
 }
