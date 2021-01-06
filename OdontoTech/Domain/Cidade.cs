edbcc43b8973eedb5822b8f30c72c01a6868a68f
marcieleadivincula <marcieleadivincula@gmail.com>
2021-01-06 11:33:31 -0300

    Create class Cidade

---
+++ OdontoTech/Domain/Cidade.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Cidade
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public Estado Estado { get; set; }
+
+        public Cidade(int id, string nome, Estado estado)
+        {
+            Id = id;
+            Nome = nome;
+            Estado = estado;
+        }
+    }
+}
