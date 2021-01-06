3a79ca6349930add864ce21e33aa3f9a77f92ad7
VitorAntonio644 <76141274+VitorAntonio644@users.noreply.github.com>
2021-01-06 10:58:08 -0300

    Create Class Pais.cs

---
+++ OdontoTech/Domain/Pais.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    class Pais
+    {
+        public int idPais { get; set; }
+        public string nomePais { get; set; }
+    }
+}
