617776bbd0d082a283fda8475cd28fecaefb7892
VitorAntonio644 <76141274+VitorAntonio644@users.noreply.github.com>
2021-01-06 11:16:55 -0300

    Create Procediomento.cs

---
+++ OdontoTech/Domain/Procedimento.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    class Procedimento
+    {
+        public int idProcedimento { get; set; }
+        public string nomeProcedimento { get; set; }
+        public string dsProcedimento { get; set; }
+        public int idTipoProcedimento { get; set; }
+    }
+}
