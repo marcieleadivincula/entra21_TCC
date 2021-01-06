e0dd1a5338099e89ef9ba06744a4f28f8cd93929
VitorAntonio644 <76141274+VitorAntonio644@users.noreply.github.com>
2021-01-06 11:22:27 -0300

    Create Tipopagamento.cs

---
+++ OdontoTech/Domain/Tipopagamento.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    class Tipopagamento
+    {
+        public int idTipoPagamento { get; set; }
+        public string tipoPagamento { get; set; }
+    }
+}
