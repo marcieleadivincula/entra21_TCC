0864ded4b44eb6f270b59b1469af742a4cfbf576
VitorAntonio644 <76141274+VitorAntonio644@users.noreply.github.com>
2021-01-12 10:15:35 -0300

    Updaste EstadoDAL.cs (Inserir)

--- OdontoTech/DataAccessLayer/EstadoDAL.cs
+++ OdontoTech/DataAccessLayer/EstadoDAL.cs
@@ -11,6 +11,38 @@ namespace DataAccessLayer
 {
     class EstadoDAL
     {
+        public void Inserir(Estado estado)
+        {
+            SqlConnection conn = new SqlConnection(DBConfig.CONNECTION_STRING);
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = $"INSERT INTO estado (idEstado,nomeEstado,idPais) values (@idEstado,@nomeEstado,@idPais)";
+            cmd.Parameters.AddWithValue("@idEstado", estado.Id);
+            cmd.Parameters.AddWithValue("@nomeEstado", estado.Nome);
+            cmd.Parameters.AddWithValue("@idPais", estado.Pais.Id);
 
+
+            try
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                if (ex.Message.Contains("UNIQUE"))
+                {
+                    throw new Exception("Estado já cadastrado.");
+                }
+                else
+                {
+                    throw new Exception("Erro no Banco de dados. Contate o administrador.");
+                }
+            }
+            finally
+            {
+                conn.Dispose();
+            }
+
+        }
     }
 }
