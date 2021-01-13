fb5770416e4d84312565d5c5c90eea8872868c34
VitorAntonio644 <76141274+VitorAntonio644@users.noreply.github.com>
2021-01-13 13:25:15 -0300

    Update TipopProcedimento.cs

--- OdontoTech/DataAccessLayer/TipoProcedimentoDAL.cs
+++ OdontoTech/DataAccessLayer/TipoProcedimentoDAL.cs
@@ -11,6 +11,44 @@ namespace DataAccessLayer
 {
     class TipoProcedimentoDAL
     {
-        
+        /// <summary>
+        /// Insere o  TipoProcedimento no BD. Caso houver erro a função informa.
+        /// </summary>
+        /// <param name="TipoProcedimento"></param>
+        public void Inserir(TipoProcedimento TipoProcedimento)
+        {
+            SqlConnection conn = new SqlConnection(DBConfig.CONNECTION_STRING);
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = $"INSERT INTO tipoprocedimento (idTipoProcedimento,nomeTipoProcedimento,valorProcedimento) values (@idTipoProcedimento,@nomeTipoProcedimento,@valorProcedimento)";
+            cmd.Parameters.AddWithValue("@idTipoProcedimento", TipoProcedimento.Id);
+            cmd.Parameters.AddWithValue("@nomeTipoProcedimento", TipoProcedimento.Nome);
+            cmd.Parameters.AddWithValue("@valorProcedimento", TipoProcedimento.Valor);
+
+
+            try
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                if (ex.Message.Contains("UNIQUE"))
+                {
+                    throw new Exception("Tipo Procedimento já cadastrado.");
+                }
+                else
+                {
+                    throw new Exception("Erro no Banco de dados. Contate o administrador.");
+                }
+            }
+            finally
+            {
+                conn.Dispose();
+            }
+
+        }
+
+
     }
 }
