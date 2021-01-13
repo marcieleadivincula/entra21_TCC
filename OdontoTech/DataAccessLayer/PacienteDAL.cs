cae830f6d332d024f69cfd5b6692cd9999be4c97
VitorAntonio644 <76141274+VitorAntonio644@users.noreply.github.com>
2021-01-13 15:48:07 -0300

    Update PacienteDAL.cs (Atualizar, Deletar)

--- OdontoTech/DataAccessLayer/PacienteDAL.cs
+++ OdontoTech/DataAccessLayer/PacienteDAL.cs
@@ -49,5 +49,60 @@ namespace DataAccessLayer
             }
 
         }
+        public string Deletar(Paciente paciente)
+        {
+            SqlConnection conn = new SqlConnection(DBConfig.CONNECTION_STRING);
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "DELETE FROM paciente WHERE idPaciente = @ID";
+            cmd.Parameters.AddWithValue("@ID", paciente.Id);
+
+            try
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                return "Paciente deletado com êxito!";
+            }
+            catch (Exception)
+            {
+                return "Erro no Banco de dados.Contate o administrador.";
+            }
+            finally
+            {
+                conn.Dispose();
+            }
+        }
+        public string Atualizar(Paciente paciente)
+        {
+
+            SqlConnection conn = new SqlConnection(DBConfig.CONNECTION_STRING);
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "UPDATE paciente SET nome = @nome, SET sobrenome = @sobrenome, SET rg = @rg, SET cpf = @cpf, SET dtNascimento = @dtNascimento, SET obs = @obs, SET idEndereco = @idEndereco WHERE idPaciente = @idPaciente";
+            cmd.Parameters.AddWithValue("@idPaciente", paciente.Id);
+            cmd.Parameters.AddWithValue("@nome", paciente.Nome);
+            cmd.Parameters.AddWithValue("@sobrenome", paciente.Sobrenome);
+            cmd.Parameters.AddWithValue("@rg", paciente.Rg);
+            cmd.Parameters.AddWithValue("@cpf", paciente.Cpf);
+            cmd.Parameters.AddWithValue("@dtNascimento", paciente.DataNascimento);
+            cmd.Parameters.AddWithValue("@obs", paciente.Observacao);
+            cmd.Parameters.AddWithValue("@idEndereco", paciente.Endereco.Id);
+
+
+            try
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                return "Paciente atualizado com êxito!";
+            }
+            catch (Exception)
+            {
+                return "Erro no Banco de dados.Contate o administrador.";
+            }
+            finally
+            {
+                conn.Dispose();
+            }
+        }
     }
 }
