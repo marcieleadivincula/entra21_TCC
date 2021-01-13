d83ed8a058d3f41d8a775e178b7a9ca932518851
VitorAntonio644 <76141274+VitorAntonio644@users.noreply.github.com>
2021-01-13 13:17:40 -0300

    Update ClinicaDAL.cs & Clinica.cs

--- OdontoTech/DataAccessLayer/ClinicaDAL.cs
+++ OdontoTech/DataAccessLayer/ClinicaDAL.cs
@@ -76,5 +76,73 @@ namespace DataAccessLayer
             }
         }
 
+        /// <summary>
+        /// Tenta atualizar, caso der certo retorna (Clinica atualizada com êxito!) se não (Erro no Banco de dados. Contate o administrador.)
+        /// </summary>
+        /// <param name="clinica"></param>
+        /// <returns></returns>
+        public string Atualizar(Clinica clinica)
+        {
+            SqlConnection conn = new SqlConnection(DBConfig.CONNECTION_STRING);
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "UPDATE clinica SET nomeClinica = @nomeClinica, SET dtInauguracao = @dtInauguracao, SET idEndereco = @idEndereco WHERE idClinica = @idClinica";
+            cmd.Parameters.AddWithValue("@idClinica", clinica.Id);
+            cmd.Parameters.AddWithValue("@nomeClinica", clinica.nNome);
+            cmd.Parameters.AddWithValue("@dtInauguracao", Convert.ToString(clinica.DataInauguracao));
+            cmd.Parameters.AddWithValue("@idEndereco", clinica.Endereco.Id);
+
+            try
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                return "Clinica atualizada com êxito!";
+            }
+            catch (Exception)
+            {
+                return "Erro no Banco de dados.Contate o administrador.";
+            }
+            finally
+            {
+                conn.Dispose();
+            }
+        }
+        /// <summary>
+        /// Retorna Lista com todas as clinicas
+        /// </summary>
+        /// <returns></returns>
+        public List<Clinica> SelecionaTodos()
+        {
+            SqlConnection conn = new SqlConnection(DBConfig.CONNECTION_STRING);
+            SqlCommand command = new SqlCommand();
+            command.Connection = conn;
+            command.CommandText = "SELECT * FROM clinica";
+            try
+            {
+                conn.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                List<Clinica> Clinicas = new List<Clinica>();
+                while (reader.Read())
+                {
+                    Clinica temp = new Clinica();
+
+                    temp.Id = Convert.ToInt32(reader["idClinica"]);
+                    temp.nNome = Convert.ToString(reader["nomeClinica"]);
+                    temp.DataInauguracao = Convert.ToDateTime(reader["dtInauguracao"]);
+                    temp.Endereco.Id = Convert.ToInt32(reader["idEndereco"]);
+
+                    Clinicas.Add(temp);
+                }
+                return Clinicas;
+            }
+            catch (Exception)
+            {
+                throw new Exception("Erro no Banco de dados.Contate o administrador.");
+            }
+            finally
+            {
+                conn.Dispose();
+            }
+        }
     }
 }
