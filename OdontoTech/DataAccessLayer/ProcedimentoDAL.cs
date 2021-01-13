5b7b7a83e6a3175683746cf6ac9874574dc1e575
VitorAntonio644 <76141274+VitorAntonio644@users.noreply.github.com>
2021-01-13 12:36:43 -0300

    Update ProcedimentoDAL.cs (INSERIR)

--- OdontoTech/DataAccessLayer/ProcedimentoDAL.cs
+++ OdontoTech/DataAccessLayer/ProcedimentoDAL.cs
@@ -11,5 +11,44 @@ namespace DataAccessLayer
 {
     class ProcedimentoDAL
     {
+        /// <summary>
+        /// Inserir procedimento
+        /// </summary>
+        /// <param name="procedimento"></param>
+        public void Inserir(Procedimento procedimento)
+        {
+            SqlConnection conn = new SqlConnection(DBConfig.CONNECTION_STRING);
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = $"INSERT INTO procedimento (idProcedimento,nomeProcedimento,dsProcedimento,idTipoProcedimento) values (@idProcedimento,@nomeProcedimento,@dsProcedimento,@idTipoProcedimento)";
+            cmd.Parameters.AddWithValue("@idProcedimento", procedimento.Id);
+            cmd.Parameters.AddWithValue("@nomeProcedimento", procedimento.Nome);
+            cmd.Parameters.AddWithValue("@dsProcedimento", procedimento.DescricaoProcedimento);
+            cmd.Parameters.AddWithValue("@idTipoProcedimento", procedimento.TipoProcedimento.Id);
+
+
+            try
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                if (ex.Message.Contains("UNIQUE"))
+                {
+                    throw new Exception("Procedimento já cadastrado.");
+                }
+                else
+                {
+                    throw new Exception("Erro no Banco de dados. Contate o administrador.");
+                }
+            }
+            finally
+            {
+                conn.Dispose();
+            }
+
+        }
+
     }
 }
