e2f5f058a22339f36367cd6c26e99de2e871a8ff
VitorAntonio644 <76141274+VitorAntonio644@users.noreply.github.com>
2021-01-13 18:36:15 -0300

    Update UsuarioDAL.cs (Atualizar & SelecionarTodos)

--- OdontoTech/DataAccessLayer/UsuarioDAL.cs
+++ OdontoTech/DataAccessLayer/UsuarioDAL.cs
@@ -66,5 +66,66 @@ namespace DataAccessLayer
                 conn.Dispose();
             }
         }
+        public string Atualizar(Usuario usuario)
+        {
+
+            SqlConnection conn = new SqlConnection(DBConfig.CONNECTION_STRING);
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "UPDATE usuario SET login = @login, SET senha = @senha, SET idColaborador = @idColaborador WHERE idUsuario = @idUsuario";
+            cmd.Parameters.AddWithValue("@idUsuario", usuario.Id);
+            cmd.Parameters.AddWithValue("@login", usuario.Login);
+            cmd.Parameters.AddWithValue("@senha", usuario.Senha);
+            cmd.Parameters.AddWithValue("@idColaborador", usuario.Colaborador.Id);
+
+            try
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                return "Usuario atualizado com êxito!";
+            }
+            catch (Exception)
+            {
+                return "Erro no Banco de dados.Contate o administrador.";
+            }
+            finally
+            {
+                conn.Dispose();
+            }
+
+        }
+        public List<Usuario> SelecionaTodos()
+        {
+            SqlConnection conn = new SqlConnection(DBConfig.CONNECTION_STRING);
+            SqlCommand command = new SqlCommand();
+            command.Connection = conn;
+            command.CommandText = "SELECT * FROM usuario";
+            try
+            {
+                conn.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                List<Usuario> Usuarios = new List<Usuario>();
+                while (reader.Read())
+                {
+                    Usuario temp = new Usuario();
+
+                    temp.Id = Convert.ToInt32(reader["idUsuario"]);
+                    temp.Login = Convert.ToString(reader["login"]);
+                    temp.Senha = Convert.ToString(reader["senha"]);
+                    temp.Colaborador.Id = Convert.ToInt32(reader["idColaborador"]);
+
+                    Usuarios.Add(temp);
+                }
+                return Usuarios;
+            }
+            catch (Exception)
+            {
+                throw new Exception("Erro no Banco de dados.Contate o administrador.");
+            }
+            finally
+            {
+                conn.Dispose();
+            }
+        }
     }
 }
