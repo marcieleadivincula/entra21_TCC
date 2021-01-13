d5f92e623a2d484b6b2338ff675487418bf8821b
VitorAntonio644 <76141274+VitorAntonio644@users.noreply.github.com>
2021-01-13 13:40:23 -0300

    Update TipoProcedimentoDAL.cs (Atualizar))

--- OdontoTech/DataAccessLayer/TipoProcedimentoDAL.cs
+++ OdontoTech/DataAccessLayer/TipoProcedimentoDAL.cs
@@ -77,5 +77,37 @@ namespace DataAccessLayer
                 conn.Dispose();
             }
         }
+        /// <summary>
+        /// Tenta atualizar, caso der certo retorna (TipoProcedimento atualizado com êxito!) se não (Erro no Banco de dados. Contate o administrador.)
+        /// </summary>
+        /// <param name="TipoProcedimento"></param>
+        /// <returns></returns>
+        public string Atualizar(TipoProcedimento TipoProcedimento)
+        {
+
+            SqlConnection conn = new SqlConnection(DBConfig.CONNECTION_STRING);
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "UPDATE tipoprocedimento SET nomeTipoProcedimento = @nomeTipoProcedimento, SET valorProcedimento = @valorProcedimento WHERE idTipoProcedimento = @idTipoProcedimento";
+            cmd.Parameters.AddWithValue("@nomeTipoProcedimento", TipoProcedimento.Nome);
+            cmd.Parameters.AddWithValue("@valorProcedimento", TipoProcedimento.Valor);
+            cmd.Parameters.AddWithValue("@idTipoProcedimento", TipoProcedimento.Id);
+
+            try
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                return "TipoProcedimento atualizado com êxito!";
+            }
+            catch (Exception)
+            {
+                return "Erro no Banco de dados.Contate o administrador.";
+            }
+            finally
+            {
+                conn.Dispose();
+            }
+
+        }
     }
 }
