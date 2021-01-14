e1a8b0d032905a867b111f7bcf7104ca861c5d79
VitorAntonio644 <76141274+VitorAntonio644@users.noreply.github.com>
2021-01-14 10:34:45 -0300

    Update PaisDAL.cs
    Inserir

--- OdontoTech/DataAccessLayer/PaisDAL.cs
+++ OdontoTech/DataAccessLayer/PaisDAL.cs
@@ -11,5 +11,35 @@ namespace DataAccessLayer
 {
     public class PaisDAL
     {
+        public void Inserir(Pais pais)
+        {
+            SqlConnection conn = new SqlConnection(DBConfig.CONNECTION_STRING);
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = $"INSERT INTO pais (nomePais) values (@nomePais)";
+            cmd.Parameters.AddWithValue("@nomePais", pais.Nome);
+
+            try
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                if (ex.Message.Contains("UNIQUE"))
+                {
+                    throw new Exception("Pais já cadastrado.");
+                }
+                else
+                {
+                    throw new Exception("Erro no Banco de dados. Contate o administrador.");
+                }
+            }
+            finally
+            {
+                conn.Dispose();
+            }
+
+        }
     }
 }
