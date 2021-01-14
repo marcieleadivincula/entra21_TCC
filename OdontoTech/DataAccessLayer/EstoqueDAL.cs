fb432e595769b9b3522836a30878d7789c8f8959
VitorAntonio644 <76141274+VitorAntonio644@users.noreply.github.com>
2021-01-14 10:55:11 -0300

    Update EstoqueDAL.cs
    Iserir

--- OdontoTech/DataAccessLayer/EstoqueDAL.cs
+++ OdontoTech/DataAccessLayer/EstoqueDAL.cs
@@ -11,5 +11,40 @@ namespace DataAccessLayer
 {
     class EstoqueDAL
     {
+        public void Inserir(Estoque Estoque)
+        {
+            SqlConnection conn = new SqlConnection(DBConfig.CONNECTION_STRING);
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = $"INSERT INTO estoque (idProduto,qtdProduto,dtEntrada,dtSaida) values (@idProduto,@qtdProduto,@dtEntrada@dtSaida)";
+
+            cmd.Parameters.AddWithValue("@idProduto", Estoque.Produto.Id);
+            cmd.Parameters.AddWithValue("@qtdProduto", Estoque.QtdProduto);
+            cmd.Parameters.AddWithValue("@dtEntrada", Estoque.DataEntrada);
+            cmd.Parameters.AddWithValue("@dtSaida", Estoque.DataSaida); 
+
+
+            try
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                if (ex.Message.Contains("UNIQUE"))
+                {
+                    throw new Exception("Estoque já cadastrado.");
+                }
+                else
+                {
+                    throw new Exception("Erro no Banco de dados. Contate o administrador.");
+                }
+            }
+            finally
+            {
+                conn.Dispose();
+            }
+
+        }
     }
 }
