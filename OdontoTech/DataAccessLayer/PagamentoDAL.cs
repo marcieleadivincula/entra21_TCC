72e93de4a63d8386b07a409e0d13ac42b5f03181
VitorAntonio644 <76141274+VitorAntonio644@users.noreply.github.com>
2021-01-14 11:17:41 -0300

    Update PagamentoDAL.cs
    Inserir

--- OdontoTech/DataAccessLayer/PagamentoDAL.cs
+++ OdontoTech/DataAccessLayer/PagamentoDAL.cs
@@ -11,5 +11,38 @@ namespace DataAccessLayer
 {
     class PagamentoDAL
     {
+        public void Inserir(Pagamento Pagamento)
+        {
+            SqlConnection conn = new SqlConnection(DBConfig.CONNECTION_STRING);
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = $"INSERT INTO pagamento (dtPagamento,idTipoPagamento) values (@dtPagamento,@idTipoPagamento)";
+
+            cmd.Parameters.AddWithValue("@idTipoPagamento", Pagamento.TipoPagamento.Id);
+            cmd.Parameters.AddWithValue("@dtPagamento", Pagamento.DataPagamento);
+
+
+            try
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                if (ex.Message.Contains("UNIQUE"))
+                {
+                    throw new Exception("Pagamento já cadastrado.");
+                }
+                else
+                {
+                    throw new Exception("Erro no Banco de dados. Contate o administrador.");
+                }
+            }
+            finally
+            {
+                conn.Dispose();
+            }
+
+        }
     }
 }
