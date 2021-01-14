6abee84d14b4dadc9c8816276a1a8cf6603d35a1
VitorAntonio644 <76141274+VitorAntonio644@users.noreply.github.com>
2021-01-14 10:13:27 -0300

    Create CidadeDAL.cs

---
+++ OdontoTech/DataAccessLayer/CidadeDAL.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer;
+using System.Data.SqlClient;
+using Domain;
+
+namespace DataAccessLayer
+{
+    public class CidadeDAL
+    {
+
+    }
+}
