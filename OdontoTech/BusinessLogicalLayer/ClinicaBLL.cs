e158d76d94b1fac3b2cc438e4993ac1e475a5f1d
Roberto Jr <absolublu14@gmail.com>
2021-01-25 10:23:00 -0300

    Logradouro, Paciente, Pais e Produtos
    criado alguns e modificado outros!!

--- OdontoTech/BusinessLogicalLayer/ClinicaBLL.cs
+++ OdontoTech/BusinessLogicalLayer/ClinicaBLL.cs
@@ -1,5 +1,5 @@
-﻿using DataAccessLayer;
-using Domain;
+﻿    using DataAccessLayer;
+    using Domain;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -44,7 +44,47 @@ namespace BusinessLogicalLayer
             return respostaDB;
         }
 
+        public List<Clinica> GetAll()
+        {
+            return dal.SelecionaTodos();
+        }
 
+        public string Update(Clinica clinica)
+        {
+            StringBuilder erros = new StringBuilder();
 
+            if (string.IsNullOrWhiteSpace(clinica.Nome))
+            {
+                erros.AppendLine("O nome da clínica deve ser informado.");
+            }
+
+            if (clinica.Nome.Length > 60)
+            {
+                erros.AppendLine("O nome não pode conter mais que 60 caracteres.");
+            }
+
+            if (clinica.DataInauguracao == null) // rever a data
+            {
+                erros.AppendLine("A data de inauguração da clínica deve ser informado.");
+            }
+
+            if (string.IsNullOrWhiteSpace(clinica.Nome))
+            {
+                erros.AppendLine("O nome da clínica deve ser informado.");
+            }
+
+            if (erros.Length != 0)
+            {
+                return erros.ToString();
+            }
+            string respostaDB = dal.Inserir(clinica);
+            return respostaDB;
+        }
+
+        public string Delete(Clinica clinica)
+        {
+            string respostaDB = dal.Deletar(clinica);
+            return respostaDB;
+        }
     }
 }
