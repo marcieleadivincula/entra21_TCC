b3792b4f2f266a9e8b074845e7e0d955a43cf993
Roberto Jr <absolublu14@gmail.com>
2021-01-26 16:21:59 -0300

    Public em TipoEmbalagemBLL
    alterado para public.

--- OdontoTech/BusinessLogicalLayer/TipoEmbalagemBLL.cs
+++ OdontoTech/BusinessLogicalLayer/TipoEmbalagemBLL.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace BusinessLogicalLayer
 {
-    class TipoEmbalagemBLL
+    public class TipoEmbalagemBLL
     {
         TipoEmbalagemDAL dal = new TipoEmbalagemDAL();
 
