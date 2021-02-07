f6316e027c76bcc00740f69caa452c092912f5db
marcieleadivincula <marcieleadivincula@gmail.com>
2021-02-07 15:59:32 -0300

    Criado IActionResult Views

--- OdontoTech/PresentationLayer/Controllers/HomeController.cs
+++ OdontoTech/PresentationLayer/Controllers/HomeController.cs
@@ -18,12 +18,141 @@ namespace PresentationLayer.Controllers
             _logger = logger;
         }
 
+        [HttpPost]
         public IActionResult Index()
         {
+            ViewBag.name = "";
+            ViewBag.pass = "";
+
             return View();
         }
 
         public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Colaborador()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Atendimento()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Paciente()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Funcao()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Clinica()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contato()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult FoneTipo()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Estoque()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Endereco()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Logradouro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Bairro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cidade()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Estado()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Pais()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Pagamento()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Produto()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Procedimento()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult TipoEmbalagem()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult TipoPagamento()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult TipoProcedimento()
         {
             return View();
         }
